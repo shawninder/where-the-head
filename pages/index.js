2692c7852c260a27e97faf801ccf36eb64c6102c
Shawn Inder <shawninder@gmail.com>
2018-10-19 18:11:00 -0400

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <style jsx global>{`
+          body {
+            background: teal;
+            padding: 100px;
+          }
+        `}</style>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App
